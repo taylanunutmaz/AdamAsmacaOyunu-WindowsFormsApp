a58e53a27d5a5f2399a8fba53ee019b211fac5db
taylanunutmaz <taylanunutmaz@gmail.com>
2019-03-07 21:50:24 +0300

    Upload to Github

---
+++ AdamAsmacaOyunu/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace AdamAsmacaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat / Yeniden Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Oyuna Başlamak için Başlat\'a Bas";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(18, 112);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(59, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Harf Tahmin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Location = new System.Drawing.Point(18, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(124, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 26);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Kelime Tahmin";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "HATA: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "HARF LİMİTİ: 10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "JOKER: 0";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 26);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Joker Kullan";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AdamAsmacaOyunu/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmacaOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Random rd = new Random();
+        string secilen; //kelimeler arasından seçilen kelimenin tutulduğu string
+        string acilan; //seçilen kelimeden açılan harflerin tutulduğu string
+        string temp; //işlem yapılacak stringi geçici olarak tutacak string
+        int hata = 0; //hata sayısı
+        int harfLimiti = 10; //10 dan daha az tahmin yapıldığında joker vermesi için tutulan değişken
+        int joker = 0; //kazanılan joker sayısını tutan değişken
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameStart();
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        //Oyunu başlatan fonksiyon
+        private void gameStart()
+        {
+            hata = -1; //CinAliCiz methodunda ekrandaki Cin Ali'yi temizlettirmek için kullanılır
+            cinAliCiz(); //hata değişkeni bu methoddan sonra 0 olur.
+            acilan = "";
+            hata = 0; //hata sıfırlanır
+            harfLimiti = 10; //harf limiti yeniden belirlenir
+            label2.Text = "HATA: " + hata.ToString();
+            label3.Text = "HARF LİMİTİ: " + harfLimiti.ToString();
+
+            //random olarak seçilecek kelimelerin listesi
+            string[] words =
+            {
+                "PORTAKAL",
+                "BARDAK",
+                "MUSTAFA",
+                "PENCERE",
+                "PARLAK",
+                "KUTU",
+                "KUMANDA",
+                "TELEFON",
+                "BARDAK",
+                "EKRAN",
+            };
+
+            secilen = words[rd.Next(0, secilen.Length)]; //random olarak kelime seçer
+
+            //seçilen kelimin harf sayısı kadar "_" karakterini aralarında " " karakteri
+            //olmak üzere label1 e yazılır.
+            label1.Text = "";
+            for (int i = 0; i < secilen.Length; i++)
+            {
+                label1.Text += "_ ";
+                acilan += ' ';
+            }
+        }
+
+        //textBox1'e harf girildiyse harfKontrol methodu çalıştırılır
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") harfKontrol();
+        }
+
+
+        private void harfKontrol()
+        {
+            char harf = Convert.ToChar(textBox1.Text);
+
+            if (secilen.Contains(harf)) //girilen harf seçilen kelimede var ise label1 yeniden yazılır
+            {
+                label1.Text = "";
+                for (int i = 0; i < secilen.Length; i++)
+                {
+                    if (secilen[i] == harf) //seçilen kelimede o har var ise
+                    {
+                        label1.Text += harf + " "; //harf ve bir boşluk ekrana yazılır 
+                        yerlestir(i, harf); //açılan stringinin i. indisine açılan harf yerleştirilir
+                    }
+                    else if (acilan[i] != ' ') //harf daha önceden açılmış ise
+                    {
+                        label1.Text += acilan[i] + " "; //açılan harf bir boşluk ile yazılır.
+                    }
+                    else //harf dana önce açılmamışsa
+                    {
+                        label1.Text += "_ "; //"_" karakteri bir boşluk ile beraber yazılır.
+                    }
+                }
+            }
+            else //eğer girilen harf seçilen kelimede yoksa 
+            {
+                hata++; //hata sayısı artar
+                label2.Text = "HATA: " + hata.ToString(); //gerekli label güncellenir
+                cinAliCiz(); //cin alinin bir parçası daha çizilir
+            }
+
+            harfLimiti--; //joker vermen için tahmine getirilen harf limiti güncellenir
+            label3.Text = "HARF LİMİTİ: " + harfLimiti.ToString();
+            oyunBittiMi();
+            textBox1.Clear();
+        }
+
+        // Oyunun bitip bitmediğini kontrol eden method
+        public void oyunBittiMi()
+        {
+            // yapılan hata 10 a ulaşınca oyun sona erer
+            if (hata >= 10)
+            {
+                MessageBox.Show("Oyunu Kaybettiniz");
+                gameStart(); // oyunu yeniden başlatır
+            }
+            else  
+            {
+                //yapılan hata sayısı 10 dan küçük ise ve seçilen kelimedeki harflerin tamamı açıldıysa
+                //kazandınız mesajı gösterilir ve oyun yeniden başlar
+                if (secilen == acilan)
+                {
+                    //harf limiti aşılmadıysa yeni joker eklenir
+                    if (harfLimiti >= 0) {
+                        joker++;
+                        button4.Enabled = true;
+                    } 
+                    label4.Text = "JOKER: " + joker; //label4'ü günceller
+                    MessageBox.Show("Kazandınız Tebrikler!\nHata Sayısı: " + hata.ToString());
+                    gameStart();
+                    
+                }
+            }
+        }
+
+        // Kelime tahmini yapıldığında seçilen kelime ile eşleşiyorsa oyunu kazanırsınız
+        // Aksi halde oyunun kaybedersiniz ve oyun biter ve oyun yeniden başlar
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == secilen)
+            {
+                if (harfLimiti >= 0) {
+                    joker++;
+                    button4.Enabled = true;
+                }
+                label4.Text = "JOKER: " + joker;
+                MessageBox.Show("Kazandınız Tebrikler!\nHata Sayısı: " + hata.ToString());
+                gameStart();
+            }
+            else
+            {
+                while (hata != 10)//Cin alinin tamamı çizilebilmesi için bu döngü kullanılır
+                {
+                    hata++;
+                    cinAliCiz();
+                }
+                label2.Text = "HATA: " + hata.ToString();
+                oyunBittiMi(); // hata 10'a ulaştığı için oyun biter
+            }
+            textBox2.Clear();
+        }
+
+        // Adam asmaca oyununun adamını asan method
+        private void cinAliCiz() 
+        {
+            Graphics g = this.CreateGraphics();
+            Pen p = new Pen(Color.Black, 5);
+            if (hata == -1)
+            {
+                g.Clear(this.BackColor);
+                hata = 0;
+            }
+            if (hata == 1) g.DrawLine(p, 500, 400, 650, 400);
+            else if (hata == 2) g.DrawLine(p, 525, 400, 525, 200);
+            else if (hata == 3) g.DrawLine(p, 525, 200, 575, 200);
+            else if (hata == 4) g.DrawLine(p, 575, 200, 575, 220);
+            else if (hata == 5) g.DrawEllipse(p, 560, 220, 30, 30);
+            else if (hata == 6) g.DrawLine(p, 575, 250, 575, 330);
+            else if (hata == 7) g.DrawLine(p, 575, 250, 545, 280);
+            else if (hata == 8) g.DrawLine(p, 575, 250, 605, 280);
+            else if (hata == 9) g.DrawLine(p, 575, 325, 545, 355);
+            else if (hata == 10) g.DrawLine(p, 575, 325, 605, 355);
+        }
+
+        //açılan stringine, a karakteri verilen indise yerleştirilir
+        private void yerlestir(int indis, char a)
+        {
+            temp = acilan; 
+            acilan = ""; 
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (i == indis) acilan += a; 
+                else acilan += temp[i]; 
+            }
+        }
+
+        // Joker kullanma butonu
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (joker > 0) //joker var ise
+            {
+                int x;
+                //daha önce açılmamış bir harf indisi bulur
+                do
+                {
+                    x = rd.Next(0, secilen.Length);
+                } while (acilan[x] != ' ');
+
+                //harfKontrol methodundaki gibi gerekli harfi açar
+                label1.Text = "";
+                for (int i = 0; i < secilen.Length; i++)
+                {
+                    if (secilen[i] == secilen[x])
+                    {
+                        label1.Text += secilen[x] + " ";
+                        yerlestir(i, secilen[x]);
+                    }
+                    else if (acilan[i] != ' ')
+                    {
+                        label1.Text += acilan[i] + " ";
+                    }
+                    else
+                    {
+                        label1.Text += "_ ";
+                    }
+                }
+                joker--;
+                label4.Text = "JOKER: " + joker;
+            }
+            else MessageBox.Show("Yeterli jokeriniz yok.");
+        }
+    }
+}
